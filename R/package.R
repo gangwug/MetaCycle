af5f27aff7848d46a77055cb28caed54dc1f1390
Gang Wu <wggucas@gmail.com>
2015-11-18 17:06:44 -0500

    MetaCycle

---
+++ R/package.R
@@ -0,0 +1,13 @@
+##' MetaCycle
+##' An integrated R package to evaluate periodicity in large scale data
+##' @name MetaCycle
+##' @docType package
+##' @author
+##' Gang Wu \email{wggucas@@gmail.com},
+##' Ron Anafi \email{ranafi@@gmail.com},
+##' John Hogenesch \email{hogenesch@@gmail.com},
+##' Michael Hughes \email{michael.evan.hughes@@gmail.com},
+##' Karl Kornacker \email{kornacker@@midohio.twcbc.com}
+##' Maintainer: Gang Wu \email{wggucas@@gmail.com}
+NULL
+
