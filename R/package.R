c74968dea53a9cff0852555fa1044dcc3d55d296
Gang Wu <wggucas@gmail.com>
2019-04-03 15:14:01 -0400

    Update package

--- R/package.R
+++ R/package.R
@@ -4,12 +4,12 @@
 ##' @docType package
 ##' @author
 ##' Gang Wu \email{wggucas@@gmail.com},
-##' Xavier Li \email{haohan.li.97@@gmail.com},
-##' Matthew Carlucci \email{matt.carlucci.96@@gmail.com},
 ##' Ron Anafi \email{ranafi@@gmail.com},
 ##' John Hogenesch \email{hogenesch@@gmail.com},
 ##' Michael Hughes \email{michael.evan.hughes@@gmail.com},
-##' Karl Kornacker \email{kornacker@@midohio.twcbc.com}
+##' Karl Kornacker \email{kornacker@@midohio.twcbc.com},
+##' Xavier Li \email{haohan.li.97@@gmail.com},
+##' Matthew Carlucci \email{matt.carlucci.96@@gmail.com}
 ##' Maintainer: Gang Wu \email{wggucas@@gmail.com}
 NULL
 
